e7e41d561b9c0d33f56db30b9867ce62bd69e516
MasalimovTimur <masalimovtr@gmail.com>
2024-12-12 20:39:48 +0500

    Task1

--- Tyuiu.MasalimovTR.Sprint6.Task1.V2.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint6.Task1.V2.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MasalimovTR.Sprint6.Task1.V2.Lib
         public double[] GetMassFunction(int startValue, int stopValue)
         {
             startValue =1 ; stopValue =1;
-            return new double[0];
+            return [-15.6, -12.35, -10.72, -8.88, -2.16, 3.25, 6.64, 10.3, 16.17, 28.1, 38.41];
         }
     }
 }
