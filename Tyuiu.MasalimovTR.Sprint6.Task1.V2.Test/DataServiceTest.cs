860aa0308e97f434294008987d5ad19fe4c211ec
MasalimovTimur <masalimovtr@gmail.com>
2024-12-12 20:32:49 +0500

    Task1

---
+++ Tyuiu.MasalimovTR.Sprint6.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MasalimovTR.Sprint6.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
